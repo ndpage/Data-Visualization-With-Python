798c44a316580a28aba68637d0e4dbee876db4a0
Nathan Page <Nathan.Page@boschrexroth-us.com>
2022-01-05 17:04:51 -0500

    Added week4 directory

--- week3-advanced-viz/worldmap.py
+++ week3-advanced-viz/worldmap.py
@@ -2,6 +2,8 @@
 import webbrowser
 from branca import colormap
 import folium
+import pandas as pd
+import numpy as np
 
 class Map:
     def __init__(self, center, zoom_start):
@@ -20,18 +22,19 @@ class Map:
             folium.CircleMarker(coords, radius=3, color = 'red')
         )
         folium.Marker(coords,popup=cityname).add_to(self.my_map)
-    def addMarker(self, coords):
+    def __addMarker(self, coords):
         folium.Marker(coords).add_to(self.my_map)
         
 
 #Define coordinates of where we want to center our map
+
+df_incidents = pd.read_csv('https://cf-courses-data.s3.us.cloud-object-storage.appdomain.cloud/IBMDeveloperSkillsNetwork-DV0101EN-SkillsNetwork/Data%20Files/Police_Department_Incidents_-_Previous_Year__2016_.csv')
+
+print(df_incidents.head())
+
 marker_coords = [51.5074, 0.1278] # London UK
 coords = (34.852619, -82.394012) 
 
-map = Map(center = coords, zoom_start = 2)
-
-map.addCity(cityname='Greenville', coords = coords)
-
-map.addMarker(marker_coords)
-
-map.showMap()
+#map = Map(center = coords, zoom_start = 2)
+#map.addCity(cityname='Greenville', coords = coords)
+#map.showMap()

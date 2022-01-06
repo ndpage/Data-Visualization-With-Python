df42706278f0cba4b212e7d74110fbf4ed21bf54
Nathan Page <33336363+ndpage@users.noreply.github.com>
2022-01-05 21:58:21 -0500

    Installed modules and added data ingestion

--- week4-dashboard/dashboard.py
+++ week4-dashboard/dashboard.py
@@ -1,2 +1,12 @@
 import plotly.graph_objects as go
-import 
+import plotly.express as px
+import numpy as np
+import pandas as pd
+
+airline_data =  pd.read_csv('https://cf-courses-data.s3.us.cloud-object-storage.appdomain.cloud/IBMDeveloperSkillsNetwork-DV0101EN-SkillsNetwork/Data%20Files/airline_data.csv', 
+                            encoding = "ISO-8859-1",
+                            dtype={'Div1Airport': str, 'Div1TailNum': str, 
+                                   'Div2Airport': str, 'Div2TailNum': str})
+
+print(airline_data.head())
+

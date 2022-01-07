d927aaf743a730412597ceaac8ef127b56e758fa
Nathan Page <Nathan.Page@boschrexroth-us.com>
2022-01-07 17:11:22 -0500

    Added code to make dashboads interactive

--- week4-dashboard/airline_data.py
+++ week4-dashboard/airline_data.py
@@ -4,7 +4,7 @@ import dash
 from dash import html
 from dash import dcc
 
-app = dash.Dash(__name__)
+
 
 # Read the airline data into pandas dataframe
 airline_data =  pd.read_csv('https://cf-courses-data.s3.us.cloud-object-storage.appdomain.cloud/IBMDeveloperSkillsNetwork-DV0101EN-SkillsNetwork/Data%20Files/airline_data.csv', 
@@ -19,13 +19,24 @@ data = airline_data.sample(n=1000, random_state=42)
 fig = px.pie(data, values='Flights', names='DistanceGroup', title='Distance group proportion by flights')
 
 # Main app that renders objects
+app = dash.Dash(__name__)
+
 app.layout = html.Div(children=[
     html.H1('Airline Dashboard',
         style={'textAlign': 'center', 'color': '#503D36', 'font-size': 40}),
     html.P('Proportion of distance group (250 mile distance interval group) by flights.', style={'textAlign':'center', 'color': '#F57241'}),
-    dcc.Graph(figure=fig)
+    dcc.Graph(figure=fig),
+    html.Div(["Input: ", dcc.Input(id='input-yr', value='2010', type='number')]),
+    html.Br(),
+    html.Br(),
+    html.Div([dcc.Graph(id='bar-plot')]),
 ])
 
+@app.callback(Output(coponent_id = 'bar-plot', component_property='figure'), 
+              Input(component_id='input-yr', component_property='value'))
+
+def get_graph(entered_year):
+    
 
 if __name__ == '__main__':
     app.run_server()
